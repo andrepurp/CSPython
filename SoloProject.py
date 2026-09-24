ab32a87996cb6fc88b568769d14b63e00ed8b902
André Almeida <165695802+andrepurp@users.noreply.github.com>
2026-09-24 16:14:02 +0100

    Updates

--- SoloProject.py
+++ SoloProject.py
@@ -7,9 +7,16 @@ from reportlab.lib.styles import getSampleStyleSheet
 utilizadores = []
 ips = []
 mensagens = []
+data = []
 
-padrao = 
-
+padrao = r"for\s+(\S+)\s+from\s+(\S+)"
+padraodata = r"(\w{3}\s+\d{1,2}\s+\d{2}.\d{2}.\d{2})"
 with open('auth.log', 'r', encoding='utf-8') as ficheiro:
     for linha in ficheiro:
-        print(linha.strip())
+        resultado = re.search(padrao, linha)
+        resultadodata = re.search(padraodata, linha)
+        utilizadores.append(resultado.group(1))
+        ips.append(resultado.group(2))
+        mensagens.append(linha.strip())
+        data.append(resultadodata.group(1))
+        print(f"Utilizador: {resultado.group(1)} \nIP: {resultado.group(2)} \nData: {resultadodata.group(0)}\nMensagem: {linha.strip()}  \n")
